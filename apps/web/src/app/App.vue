dedd2b6ae659057dd063b27da724e0a38aa11e37
Andrew Molyuk <andrew@molyuk.com>
2025-09-16 21:01:01 +0300

    feat: integrate Tailwind CSS and update project configuration

--- apps/web/src/app/App.vue
+++ apps/web/src/app/App.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+import './styles/tailwind.css'
+
+</script>
 <template>
-  <h1>Hello Bunderlog!</h1>
+  <h1 class="bg-blue-900 text-white p-5">Hello Bunderlog!</h1>
 </template>
