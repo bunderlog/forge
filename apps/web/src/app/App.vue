02fb600c12f624b88b4cf52117c4d09c1e2d3064
Andrew Molyuk <andrew@molyuk.com>
2025-09-20 22:50:00 +0300

    feat: add button component with variants and utility functions
    - Created a new button component (`iButton.vue`) with support for variants and sizes using `reka-ui`.
    - Implemented button variants using `class-variance-authority` for styling.
    - Added utility function `cn` for merging class names with Tailwind CSS.
    - Updated TypeScript configuration to include base URL and paths for easier imports.
    - Added new dependencies for class variance authority, clsx, and tailwind-merge.
    - Created a components configuration file for shadcn-vue integration.

--- apps/web/src/app/App.vue
+++ apps/web/src/app/App.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts">
 import './styles/tailwind.css'
-
+import { iButton } from '@/shared/ui'
 </script>
 <template>
   <h1 class="bg-blue-900 text-white p-5">Hello Bunderlog!</h1>
+  <div class="p-5">
+    <iButton variant="default">Hello Button</iButton>
+    <iButton variant="destructive">Destructive Button</iButton>
+    <iButton variant="outline">Outline Button</iButton>
+    <iButton variant="secondary">Secondary Button</iButton>
+    <iButton variant="ghost">Ghost Button</iButton>
+    <iButton variant="link">Link Button</iButton>
+  </div>
 </template>
